571e09902d6f6db01523cfba0c6ec3e890473cd5
Sayazhan Onglassyn <sayazhan.onlassyn@mail.ru>
2022-12-08 01:31:37 +0600

    feat: enable movement of player

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,10 +9,12 @@ public class PlayerBehavior : MonoBehaviour
     private float vInput;
     private float hInput;
 
+    private Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,12 +23,19 @@ public class PlayerBehavior : MonoBehaviour
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * moveSpeed;
 
+        /*
         this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
         this.transform.Translate(Vector3.right * hInput * Time.deltaTime);
+        */
     }
 
     private void FixedUpdate()
     {
-        
+        float fixedDeltaTime = Time.fixedDeltaTime;
+        rigidBody.MovePosition(this.transform.position + (this.transform.forward * vInput * fixedDeltaTime) + (this.transform.right * hInput * fixedDeltaTime));
+
+        // Physics.SyncTransforms();
+
+        // rigidBody.MovePosition(this.transform.position + this.transform.right * hInput * fixedDeltaTime);
     }
 }
