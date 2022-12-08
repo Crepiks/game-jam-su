1be6117746ca5f9648b11c0cfd7c24b8edc1057d
Azat <azatuk2005@mail.ru>
2022-12-08 11:14:22 +0600

    feat: change rigitbody to character controller

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -10,45 +10,32 @@ public class PlayerBehavior : MonoBehaviour
 
     private float vInput;
     private float hInput;
-    private float mouseRotation;
-
-    private Rigidbody rigidBody;
+    private CharacterController controller;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        //float fixedDeltaTime = Time.fixedDeltaTime;
-        //rigidBody.MovePosition(
-        //  this.transform.position +
-        //(this.transform.forward * vInput * fixedDeltaTime) +
-        // (this.transform.right * hInput * fixedDeltaTime)
-        //);
-
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * moveSpeed;
-        mouseRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+       
 
         Vector3 movementDirection = new Vector3(hInput, 0, vInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * moveSpeed;
         movementDirection.Normalize();
 
-        transform.Translate(movementDirection * moveSpeed * Time.deltaTime, Space.World);
+        controller.SimpleMove(movementDirection * magnitude);
+
+        //transform.Translate(movementDirection * moveSpeed * Time.deltaTime, Space.World);
 
         if (movementDirection != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
-
-        //if (mouseRotation != 0)
-        //{
-        //    Vector3 rotation = Vector3.up * mouseRotation;
-        //    Quaternion rotationAngle = Quaternion.Euler(rotation * Time.fixedDeltaTime);
-        //    rigidBody.MoveRotation(rigidBody.rotation * rotationAngle);
-        //}
     }
 }
