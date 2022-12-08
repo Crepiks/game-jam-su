e37c36cd52b4423df9502c2049a49ccdceb7d0d1
Azat <azatuk2005@mail.ru>
2022-12-08 14:50:50 +0600

    feat: add a little broken jumping of deer

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -10,15 +10,20 @@ public class PlayerBehavior : MonoBehaviour
 
     public float moveSpeed = 10f;
     public float turnSmoothSpeed = 0.1f;
+    public float jumpSpeed = 0.1f;
 
     private float vInput;
     private float hInput;
+
+    private float ySpeed;
+    private float originalStepOffset;
 
     float turnSmoothVelocity;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        originalStepOffset = controller.stepOffset;
     }
 
     private void Update()
@@ -30,15 +35,31 @@ public class PlayerBehavior : MonoBehaviour
         Vector3 movementDirection = new Vector3(hInput, 0, vInput).normalized;
         float magnitude = Mathf.Clamp01(movementDirection.magnitude) * moveSpeed;
 
-        if (movementDirection.magnitude >= 0.1f)
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (controller.isGrounded)
         {
-            float targetAngle = Mathf.Atan2(movementDirection.x, movementDirection.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothSpeed);
+            controller.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
 
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 angleDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.SimpleMove(angleDirection.normalized * magnitude);
+            if (Input.GetButton("Jump"))
+            {
+                ySpeed = jumpSpeed;
+            }
         }
+        else
+        {
+            controller.stepOffset = 0;
+        }
+
+        float targetAngle = Mathf.Atan2(movementDirection.x, movementDirection.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothSpeed);
+
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        Vector3 angleDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        angleDirection.y = ySpeed;
+
+        controller.Move(angleDirection.normalized * magnitude * Time.deltaTime);
     }
 }
